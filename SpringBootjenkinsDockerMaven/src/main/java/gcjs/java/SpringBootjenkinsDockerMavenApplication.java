21b7138333bb7e40d0b7902e86969a183a3352a0
Administrator <Administrator@PC-20190525RICJ>
2019-06-05 20:44:04 +0800

    aa

--- SpringBootjenkinsDockerMaven/src/main/java/gcjs/java/SpringBootjenkinsDockerMavenApplication.java
+++ SpringBootjenkinsDockerMaven/src/main/java/gcjs/java/SpringBootjenkinsDockerMavenApplication.java
@@ -3,6 +3,7 @@ package gcjs.java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootjenkinsDockerMavenApplication {
 
