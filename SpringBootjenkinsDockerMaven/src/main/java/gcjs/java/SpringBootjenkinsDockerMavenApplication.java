ee4827ca0dff096628c4cc7d9efe128500c6dc62
Administrator <Administrator@PC-20190525RICJ>
2019-06-05 22:33:29 +0800

    abc

--- SpringBootjenkinsDockerMaven/src/main/java/gcjs/java/SpringBootjenkinsDockerMavenApplication.java
+++ SpringBootjenkinsDockerMaven/src/main/java/gcjs/java/SpringBootjenkinsDockerMavenApplication.java
@@ -3,7 +3,6 @@ package gcjs.java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class SpringBootjenkinsDockerMavenApplication {
 
--- SpringBootjenkinsDockerMaven/src/main/java/gcjs/java/controller/UserController.java
+++ SpringBootjenkinsDockerMaven/src/main/java/gcjs/java/controller/UserController.java
@@ -8,28 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-
 @RestController
 @RequestMapping("/userController")
 public class UserController {
 
-    //@Value("${HOSTNAME}")
-    //private String docker;
-	
-	//http://127.0.0.1:80/userController/index
     @ResponseBody
     @RequestMapping("/index")
     public Map<String,Object> index(@RequestParam(value = "count",defaultValue = "5") int threadCount){
-        //System.out.println("--------------------docker:"+docker);
-        Map<String,Object>  map = new HashMap<>();
+     
+        Map<String,Object> map = new HashMap<>();
         map.put("hello","world");
         map.put("version",8.0);
-        //map.put("docker",docker);
         long startTime=System.currentTimeMillis();
         int count = threadCount;
         while (count--!=0){
-        	
-            System.out.println("hello world232");
+            System.out.println("hello world");
         }
         long endTime=System.currentTimeMillis();
         map.put("cost_time" ,(endTime - startTime) + "ms" );
