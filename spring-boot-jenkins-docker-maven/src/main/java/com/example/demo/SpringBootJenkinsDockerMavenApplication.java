74f3eee5af5e869306dd5e645f62d0e2b2e7003a
Administrator <Administrator@PC-20190525RICJ>
2019-06-05 19:11:55 +0800

    第二次提交

--- spring-boot-jenkins-docker-maven/src/main/java/com/example/demo/SpringBootJenkinsDockerMavenApplication.java
+++ spring-boot-jenkins-docker-maven/src/main/java/com/example/demo/SpringBootJenkinsDockerMavenApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootJenkinsDockerMavenApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsDockerMavenApplication.class, args);
 	}
